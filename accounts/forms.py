e5124b654dd176db2fe0056342768006c62ea342
Monika <monika@tansam.org>
2025-09-30 16:29:48 +0530

    Done with the changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -9,6 +10,14 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'phone_number', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': ' '
+            })
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-input'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-input'}))
